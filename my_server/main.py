a286f3bcba0848d1dd85add78d75521fc5c835ad
mavvkel <mic.kusmierz@gmail.com>
2023-06-05 04:30:42 +0200

    Uploading files

--- my_server/main.py
+++ my_server/main.py
@@ -1,15 +1,29 @@
-from flask import Blueprint, current_app, redirect, render_template, send_from_directory, url_for
+from flask import Blueprint, current_app, redirect, render_template, \
+        request, send_from_directory, url_for, flash
 from flask_login import login_required, current_user
-from pathlib import Path
+from werkzeug.utils import secure_filename
+# from pathlib import Path
 from glob import glob
 import os
 
 main = Blueprint('main', __name__)
 
-@main.route('/', methods=['GET'])
+@main.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
     files_loc = os.path.join(current_app.root_path, "data")
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part in the request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(files_loc, filename))
 
     #file_list = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames if os.path.splitext(f)[1] == '.txt']
 
@@ -29,6 +43,7 @@ def index():
     return render_template(template_name_or_list='index.html',
                            files=files,
                            name=current_user.name)
+
 
 @main.route('/download/<path:filename>')
 @login_required
